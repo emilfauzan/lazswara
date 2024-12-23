d0f59a03fea46da5c801acc9f1bdde87386c0558
Emil Fauzan <emilfauzan@gmail.com>
2024-12-23 18:43:55 +0700

    modified active navbar & nav link + several raw contents

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -1,4 +1,54 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-<h3>Program Page</h3>
+    <h3>Program</h3>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Pendidikan</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Program Pendidikan</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, voluptate.
+            Repellat non dolore quidem numquam delectus magni? Quis, mollitia quos? Laudantium totam impedit nostrum
+            exercitationem?</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Kesehatan</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Program Kesehatan</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid esse sint, iste
+            facilis soluta voluptatibus voluptatum? Fugiat nostrum commodi aspernatur, quaerat ea iure consequatur esse!
+        </p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Kemanusiaan</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Program Kemanusiaan</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, recusandae facilis?
+            Obcaecati fugit, inventore accusantium commodi modi ipsa, voluptatum hic dolorum quia non quaerat ullam.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Ekonomi</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Program Ekonomi</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore delectus aliquid
+            ipsa dolorum maxime eum libero, distinctio assumenda, ratione iste quis porro nemo pariatur dolores.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Dakwah</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Program Dakwah</a> | 1 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facere dolores
+            obcaecati necessitatibus adipisci possimus reiciendis deserunt, labore odio consectetur cum odit nam saepe
+            quibusdam.</p>
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+
 </x-layout>
