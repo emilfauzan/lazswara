ed3621bacba49387cd1fd9389a48af1ed231e005
Emil Fauzan <emilfauzan@gmail.com>
2024-12-26 13:44:06 +0700

    updated layanan flyout menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,10 +12,64 @@
                         <x-nav-link href='/about' :active="request()->is('about')">Tentang Kami</x-nav-link>
                         <x-nav-link href='/program' :active="request()->is('program')">Program</x-nav-link>
                         <x-nav-link href='/zakat' :active="request()->is('zakat')">Zakat</x-nav-link>
-                        <x-nav-link href='/layanan' :active="request()->is('layanan')">Layanan</x-nav-link>
+
+                        <div class="relative">
+                            <button @click="isOpen = !isOpen" type="button"
+                                class="inline-flex items-center gap-x-1 text-sm/6 text-white" aria-expanded="false"
+                                aria-haspopup="true" id="user-layanan-button">
+                                <x-nav-link href='#' :active="request()->is('layanan')">
+                                    <div class="flex"> Layanan
+                                        <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                                            data-slot="icon">
+                                            <path fill-rule="evenodd"
+                                                d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </x-nav-link>
+
+                            </button>
+
+                            <div x-show="isOpen" x-transition:enter="transition ease-out duration-100 transform"
+                                x-transition:enter-start="opacity-0 scale-95"
+                                x-transition:enter-end="opacity-100 scale-100"
+                                x-transition:leave="transition ease-in duration-75 transform"
+                                x-transition:leave-start="opacity-100 scale-100"
+                                x-transition:leave-end="opacity-0 scale-95"
+                                class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4"
+                                role="menu" aria-orientation="vertical" aria-labelledby="user-layanan-button"
+                                tabindex="-1">
+                                <div
+                                    class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 shadow-lg ring-1 ring-gray-900/5">
+                                    <div class="p-4">
+                                        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                                            <div>
+                                                <a href="#" class="font-semibold text-gray-900">
+                                                    Laporan
+                                                    <span class="absolute inset-0"></span>
+                                                </a>
+                                                <p class="mt-1 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, itaque?
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
+                                            <div>
+                                                <a href="#" class="font-semibold text-gray-900">
+                                                    Rekening
+                                                    <span class="absolute inset-0"></span>
+                                                </a>
+                                                <p class="mt-1 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quos!</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
+
             <div class="-mr-2 flex md:hidden">
                 <!-- Mobile menu button -->
                 <button type="button" @click="isOpen = !isOpen"
@@ -56,5 +110,40 @@
             <a href="#"
                 class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Layanan</a>
         </div>
+
+        <div class="border-t border-gray-700 pb-3 pt-4">
+            <div class="flex items-center px-5">
+                <div class="shrink-0">
+                    <img class="size-10 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="">
+                </div>
+                <div class="ml-3">
+                    <div class="text-base/5 font-medium text-white">Tom Cook</div>
+                    <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+                </div>
+                <button type="button"
+                    class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                        aria-hidden="true" data-slot="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                    </svg>
+                </button>
+            </div>
+            <div class="mt-3 space-y-1 px-2">
+                <a href="#"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+                    Profile</a>
+                <a href="#"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
+                <a href="#"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
+                    out</a>
+            </div>
+        </div>
+
     </div>
 </nav>
